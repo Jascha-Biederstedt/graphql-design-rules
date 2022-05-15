2bf649449cc35ebf324c0a92775798ee540ace77
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-15 13:39:18 +0200

    Add Rule #1

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-const typeDefs = gql`
-  type Query {
-    cars: [Car!]!
-  }
-
-  type Car {
-    id: ID!
-    color: String!
-    make: String!
-  }
-
-  type ManualGroup {
-    id: ID!
-    name: String!
-    imageId: ID!
-    bodyHtml: String!
-    memberships: [GroupMembership!]!
-  }
-
-  type AutomaticGroup {
-    id: ID!
-    name: String!
-    imageId: ID!
-    bodyHtml: String!
-    features: [AutomaticGroupFeatures!]!
-    memberships: [GroupMembership!]!
-    applyFeaturesSeperately: Boolean!
-  }
-
-  type AutomaticGroupFeatures {
-    column: String!
-  }
-
-  type GroupMembership {
-    groupId: ID!
-    carId: ID!
-  }
-`;
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./typeDefs/00-Naive-Schema');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query: {
-      cars: () => [{ id: 1, color: 'blue', make: 'Toyota' }],
+      hello: () => 'World',
     },
   },
 });

842df1c247e1a9f2d9ab7ab7ad91bd170335567d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-15 15:16:41 +0200

    Add rule #14

---
+++ typeDefs/Rule-14.js
@@ -0,0 +1,76 @@
+// Rule #14: Structure mutation inputs to reduce duplication,
+// even if this requires relaxing requiredness constraints on
+// certain fields.
+
+const { ApolloServer, gql } = require('apollo-server');
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Mutation {
+    groupDelete(groupId: ID!)
+    groupPublish(groupId: ID!)
+    groupUnpublish(groupId: ID!)
+    groupAddCars(groupId: ID!, carId: ID!)
+    groupRemoveCars(groupId: ID!, carId: ID!)
+    groupCreate(
+      groupInput: GroupInput!
+    )
+    groupUpdate(
+      groupId: ID!
+      groupInput: GroupInput!
+    )
+  }
+
+  input GroupInput {
+    name: String
+    image: ImageInput
+    description: String
+    featureSet: GroupFeatureFields
+  }
+
+  input ImageInput {
+    url: String!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    hasCar(id: ID!): Boolean!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeatures {
+    feature: GroupFeatureFields!
+  }
+
+  enum GroupFeatureFields {
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLUNDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
+  }
+`;

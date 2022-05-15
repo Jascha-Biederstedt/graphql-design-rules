af52f410fbf0d594e719236598b6ad0ffcecaecc
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-15 14:02:35 +0200

    Add Rule #4

---
+++ typeDefs/Rule-04.js
@@ -0,0 +1,30 @@
+// Rule #4: It's easier to add fields than to remove them.
+
+const { ApolloServer, gql } = require('apollo-server');
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    features: [GroupFeatures]
+    applyFeaturesSeparately: Boolean!
+    cars: [Car!]!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+  }
+
+  type GroupFeature {
+    feature: String!
+  }
+`;

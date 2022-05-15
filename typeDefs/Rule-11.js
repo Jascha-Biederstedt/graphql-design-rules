880467e2f58f17f71f54838af50cd279a4972cb2
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-15 14:43:07 +0200

    Add rule #11

---
+++ typeDefs/Rule-11.js
@@ -0,0 +1,57 @@
+// Rule #11: Write separate mutations for separate logic
+// actions on a resource.
+
+const { ApolloServer, gql } = require('apollo-server');
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String!
+    car(id: ID!): Car!
+  }
+
+  type Mutation {
+    create
+    delete
+    update
+    publish
+    unpublish
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet!
+    hasCar(id: ID!): Boolean!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    image: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type GroupFeatures {
+    feature: String!
+  }
+
+  enum GroupFeatureFields {
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLUNDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
+  }
+`;
